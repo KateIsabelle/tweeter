458ea69c7d0fbb93c7d524045d82bbe652f463f0
KateIsabelle <kate.davis887@gmail.com>
2021-03-24 18:51:30 +0000

    update user icon to use avatar and add renderTweets function into client.js for rendering tweets from db into html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,50 +4,88 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function (e) {
-  
-  const createTweetElement = function (tweetObj) {
-    return $(`
-    <article>
-    <header class="tweet-show-header">
-    <div class="username-icon">
-    <i class="fas fa-user-astronaut fa-2x"></i>
-    <div class="sml-left-padding">${tweetData.user.name}</div>
-    </div>
-    <div class="handle">${tweetData.user.handle}</div>
-    </header>
-    <section class="tweet-show-txt">${tweetData.content.text}</section>
-    <footer class="tweet-show-footer">
-    <div class="small-txt">${tweetData.created_at}</div>
-    <div class="tweet-react-icons">
-    <i class="fas fa-flag icon-react"></i>
-    <i class="fas fa-retweet icon-react"></i>
-    <i class="fas fa-heart icon-react"></i>
-    </div>
-    </footer>
-    </article>
-    `)
-  }
-
-  const renderTweets = function() {
-    const $tweet = createTweetElement(tweetData);
-    console.log($tweet); // to see what it looks like
-
-  }
-  
-});
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+//example data
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Kato Potato",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@KatoCodes"
+    },
+    "content": {
+      "text": "Wherever you go, there you are"
+    },
+    "created_at": 1461116232227
+  },
+]
 
-// Test / driver code (temporary)
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  //function takes in a tweet object, uses template literal
+  //to create html elements with tweet object info
+  //and returns html in $tweet variable
+  const createTweetElement = function(tweetObj) {
+    const $tweet = $(`
+    <article>
+      <header class="tweet-show-header">
+        <div class="username-icon">
+          <img src=${tweetObj.user.avatars} alt="user icon">
+          <div class="sml-left-padding">${tweetObj.user.name}</div>
+        </div>
+        <div class="handle">${tweetObj.user.handle}</div>
+      </header>
+      <section class="tweet-show-txt">${tweetObj.content.text}</section>
+      <footer class="tweet-show-footer">
+        <div class="small-txt">${tweetObj.created_at}</div>
+        <div class="tweet-react-icons">
+          <i class="fas fa-flag icon-react"></i>
+          <i class="fas fa-retweet icon-react"></i>
+          <i class="fas fa-heart icon-react"></i>
+        </div>
+      </footer>
+    </article>
+    `)
+
+    return $tweet;
+  }
+
+  //function loops through tweet database, 
+  //creates tweet html element for each tweet object in db array,
+  //and appends it to the #tweet-container
+  const renderTweets = function(tweets) {
+    for (const tweetN of tweets) {
+      const $tweet = createTweetElement(tweetN);
+      $('#tweets-container').append($tweet);
+    }
+
+  }
+
+$(document).ready(function(e) {
+
+renderTweets(data)
+
+});
+
+// <i class="fas fa-user-astronaut fa-2x"></i>
